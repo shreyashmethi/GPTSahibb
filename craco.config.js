e5ee40dc7dbf3be9e7fd62495fa0d7fd7c87f0c9
Shreyash Methi <100725802+shreyashmethi@users.noreply.github.com>
2024-02-11 05:46:28 +0530

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -3,7 +3,7 @@ module.exports = {
    devServer: {
      headers: {
        'Cross-Origin-Embedder-Policy': 'unsafe-none',
-       'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
+       'Cross-Origin-Opener-Policy': 'unsafe-none',
      }
    }
  }
